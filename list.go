954744217ca2665907731316dd88b2b29f43adde
jvmistica <zombossjoey@tutanota.com>
2021-02-12 22:55:37 +0800

    Add desc/asc sorting

--- list.go
+++ list.go
@@ -15,7 +15,11 @@ func (i *Items) ListItems(params string) string {
 	cmd := strings.Split(params, " ")
 	switch cmd[0] {
 	case "sort":
-		i.db.Order(cmd[2]).Find(&items)
+		if len(cmd) > 3 {
+			i.db.Order(fmt.Sprintf("%s %s", cmd[2], cmd[3])).Find(&items)
+		} else {
+			i.db.Order(cmd[2]).Find(&items)
+		}
 	case "filter":
 		i.db.Where(fmt.Sprintf("%s %s '%s'", cmd[2], cmd[3], strings.Join(cmd[4:], " "))).Find(&items)
 	default:
